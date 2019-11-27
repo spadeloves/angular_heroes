63e2a8103d7e6b2c8bc4f8ec69e3e7c02ed80593
SpadeLoves <spadeloves@gmail.com>
2019-11-27 17:42:47 +0900

    ルーティングまで

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,19 +10,12 @@ import {HeroService} from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes : Hero[];
-
-  selectedHero: Hero;
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  constructor(private heroService: HeroService) { }
 
   getHeroes() : void{
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
